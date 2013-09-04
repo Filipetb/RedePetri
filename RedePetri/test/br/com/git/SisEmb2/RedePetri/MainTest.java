a487c9aae98085a3424379243bf69872bf856a37
brunosales <brunosales@localhost.localdomain>
2013-09-04 19:19:15 -0300

    Criação do pacote teste e clase teste para o main. Criação do pacote br.com.SisEmb2.RedePetri.tela a classe TelaApp.

---
+++ RedePetri/test/br/com/git/SisEmb2/RedePetri/MainTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.git.SisEmb2.RedePetri;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author brunosales
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    public void testMain() {
+    }
+}
