18190ca1cf9a400eda7f6666a95c67180394b72a
Filipe <Filipe@Filipe-PC>
2013-09-04 16:28:22 -0300

    Commit Inicial do Projeto. (Renomeado o pacote inicial e arquivo main)

--- RedePetri/src/br/com/git/SisEmb2/RedePetri/Main.java
+++ RedePetri/src/br/com/git/SisEmb2/RedePetri/Main.java
@@ -2,13 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.git.SisEmb2;
+package br.com.git.SisEmb2.RedePetri;
 
 /**
  *
  * @author Filipe
  */
-public class RedePetri {
+public class Main {
 
     /**
      * @param args the command line arguments
