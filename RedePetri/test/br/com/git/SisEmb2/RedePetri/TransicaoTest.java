4a64540935316977c47a29b2cf669b9373392f5e
brunosales <brunosales0800@gmail.com>
2013-09-08 22:30:08 -0300

    Criação da classe Teste para a classe Trancisao. Testando métodos dessa classe

---
+++ RedePetri/test/br/com/git/SisEmb2/RedePetri/TransicaoTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.git.SisEmb2.RedePetri;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author brunosales
+ */
+public class TransicaoTest {
+    
+    public TransicaoTest() {
+    }
+
+    /**
+     * Test of Adiciona_ArcoEntrada method, of class Transicao.
+     */
+    @Test
+    public void testAdiciona_ArcoEntrada() {
+    }
+
+    /**
+     * Test of Adiciona_ArcoSaida method, of class Transicao.
+     */
+    @Test
+    public void testAdiciona_ArcoSaida() {
+    }
+
+    /**
+     * Test of getDesc_trans method, of class Transicao.
+     */
+    @Test
+    public void testGetDesc_trans() {
+    }
+
+    /**
+     * Test of setDesc_trans method, of class Transicao.
+     */
+    @Test
+    public void testSetDesc_trans() {
+    }
+}
